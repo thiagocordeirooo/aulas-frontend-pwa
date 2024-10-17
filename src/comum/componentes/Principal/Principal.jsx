6e67c9b98d185900bff3d6da13f3b473b58edc5e
Thiago Cordeiro <thiagocordeirooo@gmail.com>
2024-10-16 21:06:12 -0300

    continuação lista de tarefas

--- src/comum/componentes/Principal/Principal.jsx
+++ src/comum/componentes/Principal/Principal.jsx
@@ -1,11 +1,16 @@
 import { Link } from 'react-router-dom';
 import './Principal.css';
+import { FaChevronLeft } from 'react-icons/fa6';
 
 function Principal({ voltarPara, titulo, children }) {
   return (
     <main className="principal_root">
       <div className="principal_titulo">
-        {voltarPara && <Link to={voltarPara}>Voltar</Link>}
+        {voltarPara && (
+          <Link to={voltarPara}>
+            <FaChevronLeft size={24} color='#3f50b5' />
+          </Link>
+        )}
 
         <h1>{titulo}</h1>
       </div>
--- src/paginas/PaginaListaTarefas/PaginaListaTarefas.jsx
+++ src/paginas/PaginaListaTarefas/PaginaListaTarefas.jsx
@@ -2,22 +2,41 @@ import { useState } from 'react';
 import BotaoCustomizado from '../../comum/componentes/BotaoCustomizado/BotaoCustomizado';
 import Principal from '../../comum/componentes/Principal/Principal';
 
+import { FaTrashCan } from 'react-icons/fa6';
+
 const PaginaListaTarefas = () => {
   const [descricao, setDescricao] = useState('');
   const [tarefas, setTarefas] = useState([]);
 
   const adicionarNaLista = () => {
-    setTarefas([...tarefas, descricao]);
+    if (descricao && descricao.trim()) {
+      tarefas.push(descricao);
+      setTarefas([...tarefas]);
+    } else {
+      alert('Preencha o campo Descrição');
+    }
     setDescricao('');
+    document.getElementById('campoDescricao').focus();
+  };
+  const removerDaLista = (index) => {
+    tarefas.splice(index, 1);
+    setTarefas([...tarefas]);
   };
 
   return (
     <Principal titulo="Lista de Tarefas" voltarPara="/">
       <div>
         <input
+          id="campoDescricao"
           type="text"
+          placeholder="Descrição da tarefa"
           value={descricao}
           onChange={(e) => setDescricao(e.target.value)}
+          onKeyUp={(e) => {
+            if (e.key === 'Enter') {
+              adicionarNaLista();
+            }
+          }}
         />
         <BotaoCustomizado aoClicar={adicionarNaLista}>
           +
@@ -25,7 +44,15 @@ const PaginaListaTarefas = () => {
       </div>
       <ul>
         {tarefas.map((item, index) => {
-          return <li key={index}>{item}</li>;
+          return (
+            <li key={index}>
+              {item}
+              <FaTrashCan
+                color="red"
+                onClick={() => removerDaLista(index)}
+              />
+            </li>
+          );
         })}
       </ul>
     </Principal>
